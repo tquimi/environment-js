27fc98a52e480c67e4392929f67e6725937bbca6
tquimi <30601150+tquimi@users.noreply.github.com>
2017-08-04 14:08:01 -0500

    mocha 3

--- src/index.test.js
+++ src/index.test.js
@@ -8,15 +8,4 @@ describe('Our first test',() => {
     });
 });
 
-describe('index.html',()=> {
 
-  it('should say En contruccion', (done)=>{
-    const index = fs.readFileSync('./src/index.html',"utf-8");
-    jsdom.env(index,function(err,window){
-      const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal('En contruccion');
-      done();
-      window.close();
-    });
-  });
-});
