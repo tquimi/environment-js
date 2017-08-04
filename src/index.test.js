f489e4a7cfbab49eca46133b058f0975cd5270a4
tquimi <30601150+tquimi@users.noreply.github.com>
2017-08-04 13:40:41 -0500

    mocha 2

--- src/index.test.js
+++ src/index.test.js
@@ -14,7 +14,7 @@ describe('index.html',()=> {
     const index = fs.readFileSync('./src/index.html',"utf-8");
     jsdom.env(index,function(err,window){
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal('En contruccion!');
+      expect(h1.innerHTML).to.equal('En contruccion');
       done();
       window.close();
     });
